7b6c0acca4ec104cbe63218dbcbc9d7464b3d4ee
abhijit1998 <abhijittudu98@gmail.com>
2019-08-17 13:33:51 +0000

    typing fix

--- aicp_potter.mk
+++ aicp_potter.mk
@@ -19,7 +19,7 @@
 $(call inherit-product, device/motorola/potter/full_potter.mk)
 
 # Inherit some common CarbonRom stuff.
-$(call inherit-product, vendor/carbon/config/common_full_phone.mk)
+$(call inherit-product, vendor/aicp/config/common_full_phone.mk)
 
 # Boot animation
 TARGET_SCREEN_WIDTH := 1080
